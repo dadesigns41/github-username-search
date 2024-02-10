ead0303c69ec5da3d3ea7943a0f8a398434f41e0
dadesigns41 <dadesigns41@gmail.com>
2024-02-10 14:41:13 -0500

    added js files and api client id

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+    constructor() {
+        this.client_id = 'Iv1.9efe2db8676c52dd';
+        this.client_secret = 'd112255afeecf553b186487affb9dc52d2c2f9c5';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        
+        const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
+
+        return {
+            profile: profile ,
+            repos: repos
+        }
+    }
+}
